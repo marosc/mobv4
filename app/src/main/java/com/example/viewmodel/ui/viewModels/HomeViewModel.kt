97993ea94bd52670607a81ae40da0e219cb0be2e
Maros  Cavojsky <cavojsky.maros@gmail.com>
2019-10-16 20:23:48 +0200

    vm3

--- app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/viewmodel/ui/viewModels/HomeViewModel.kt
@@ -1,23 +1,17 @@
 package com.example.viewmodel.ui.viewModels
 
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.viewmodel.data.DataRepository
-import com.example.viewmodel.data.db.model.VideoItem
-import com.example.viewmodel.data.db.model.WordItem
-import kotlinx.coroutines.launch
+import androidx.lifecycle.*
 
 class HomeViewModel : ViewModel() {
     val input: MutableLiveData<String> = MutableLiveData()
 
     private val _word: MutableLiveData<String> = MutableLiveData()
 
+    val welcome = Transformations.map(word) { w -> "Words is: $w" }
+
     val word: LiveData<String>
         get() = _word
-
 
     fun changeWord() {
         _word.postValue(input.value)
